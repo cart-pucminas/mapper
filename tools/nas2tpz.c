5b193d40577b7f69d738dd4fb0bde4133404733c
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-02-25 11:02:53 -0300

    nas2tpz

---
+++ tools/nas2tpz.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief NAS trace filename.
+ */
+static const char *nasfile = NULL;
+
+/**
+ * @brief NoC topology.
+ */
+static struct
+{
+	unsigned nprocs;    /**< Number of processors. */
+	unsigned nrows;     /**< Number of rows.       */
+	unsigned ncols;     /**< Number of columns.    */
+} topology = { 0, 0, 0 };
+
+/**
+ * @brief Converts NAS trace file to Topaz input file.
+ */
+static void nas2tpz(FILE *nasfilep)
+{
+	float start;     /* Start of transmission. */
+	unsigned size;   /* Size of the message.   */
+	unsigned dest;   /* Destination process.   */
+	unsigned source; /* Source process.        */
+	
+	/* Convert NAS trace file. */
+	fscanf(nasfilep, "%f %*u %*c %u %u %*c %u", &start, &source, &dest, &size);
+	while (!feof(nasfilep))
+	{
+		fprintf(stdout, "%f %u %u %u %u %u %u %u\n",
+			start,
+			source%topology.ncols, source/topology.ncols, 0,
+			dest%topology.ncols, dest/topology.ncols, 0,
+			size);
+		
+		fscanf(nasfilep, "%f %*u %*c %u %u %*c %u", &start,&source,&dest,&size);
+	}
+}
+
+/**
+ * @brief Prints an error message and aborts.
+ */
+static void error(const char *msg)
+{
+	fprintf(stderr, "error: %s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * @brief Prints program usage and exits.
+ */
+static void usage(void)
+{
+	printf("Usage: nas2tpz <nas file> <nprocs> <nrows> <ncols>\n");
+	printf("Brief: converts a NAS trace file to a Topaz input file.\n");
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * @brief Reads command line arguments.
+ */
+static void readargs(int argc, char **argv)
+{	
+	/* Missing arguments. */
+	if (argc != 5)
+		usage();
+		
+	nasfile = argv[1];
+	sscanf(argv[2], "%u", &topology.nprocs);
+	sscanf(argv[3], "%u", &topology.nrows);
+	sscanf(argv[4], "%u", &topology.ncols);
+	
+	/* Bad topology. */
+	if (topology.nrows*topology.ncols != topology.nprocs)
+		error("bad topology");
+}
+
+/**
+ * @brief Converts a NAS trace file to a Topaz input file.
+ */
+int main(int argc, char **argv)
+{
+	FILE *nasfilep;
+	
+	readargs(argc, argv);
+	
+	/* Open NAS trace file. */
+	nasfilep = fopen(nasfile, "r");
+	if (nasfilep == NULL)
+		error("cannot open NAS trace file");
+		
+	nas2tpz(nasfilep);
+
+	fclose(nasfilep);
+	
+	return (EXIT_SUCCESS);
+}
