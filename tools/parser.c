a36eac81c53f3e0b07f2f5c3c564c4cfc97a6b53
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-09-04 12:03:14 -0300

    parser

---
+++ tools/parser.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+int main(int argc, char **argv)
+{
+	char rw;
+	int size;
+	uint64_t addr;
+	char line[80];
+
+	while (fgets(line, sizeof(line), stdin) != NULL)
+	{
+		sscanf(line, "%c%d%" PRIu64 "%*d", &rw, &size, &addr);
+		fprintf(stdout, "%c;%u;%" PRIx64 "\n", rw, size, addr);
+	}
+
+	return (EXIT_SUCCESS);
+}
