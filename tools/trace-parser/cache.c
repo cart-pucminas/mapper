5122194537259657d68e1a36eefaaedc0b845e9b
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-09-26 10:55:15 -0300

    enhancing cache_evict()

--- tools/trace-parser/cache.c
+++ tools/trace-parser/cache.c
@@ -21,15 +21,19 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #include "util.h"
 #include "list.h"
 #include "hash.h"
 
-#define INVALIDO_LIMPO 0;
-#define INVALIDO_SUJO 1; 
-#define VALIDO_LIMPO 2;
-#define VALIDO_SUJO 3;
+/**
+ * @brief Block flags.
+ */
+/**@{*/
+#define BLOCK_VALID (1 << 0)
+#define BLOCK_DIRTY (1 << 1)
+/**@}*/
 
 
 /**
@@ -38,7 +42,7 @@
 struct block
 {
 	void *obj;      /**< Underlying memory access. */
-	unsigned addr;	/**< Addr.                   *
+	unsigned addr;	/**< Addr.                     */
 	long offset;    /**< Offset.                   */
 	unsigned flags; /**< Flags.                    */
 	unsigned age;   /**< Age for evicting.         */
@@ -58,65 +62,54 @@ struct
 	unsigned age; 		  /**< Cache age.            */
 } cache;
 
+/**
+ * @brief Retrieves the key from a cache block.
+ */
+static unsigned getkey(void *obj)
+{
+	return (((struct block *)obj)->addr);
+}
+
 
 /**
  * @brief Evicts an object from the cache.
  */
-static void cache_evict(void)
+static void cache_evict(unsigned key)
 {
-	/* TODO. */
-	// pega o mais antigo e coloca no swap e coloca o bloco na lista de livres
+	unsigned oldest = UINT_MAX;
 	
-	int encontrar_block = 0; // tipo boolean para testar se encontrou o bloco
-	unsigned i = 0;
-	unsigned old; // age do mais antigo
-	unsigned old_blocks; // índice do mais antigo 
-	
-	struct block *block_write; //bloco que será escrito
-	struct block *block_read;  //bloco lido do arquivo para comparação com o bloco que será escrito
-	
-	//Encontrar o primeiro bloco válido para inicializar as variáveis old e old_bloks
-	while (!encontrar_block && i < cache_size){
-		
-		//Testar se o block é valido 
-		if(cache.blocks[i]->flags = VALIDO_LIMPO || cache.blocks[i]->flags = VALIDO_SUJO ){
-			//Primeiro block válido encontrado
-			old = cache.blocks[i]->age;
-			old_blocks = i;
-			encontrar_block = 1;
+	/* Search for oldest block. */
+	for (unsigned i = 0; i < cache.cache_size; i++)
+	{
+		/* Skip invalid blocks. */
+		if (!(cache.blocks->flags & BLOCK_VALID))
+			continue;
+			
+		/* First valid block. */
+		if (oldest == UINT_MAX)
+		{
+			oldest = i;
+			continue;
 		}
-		i++;
+		
+		/* Oldest block. */
+		if (cache.blocks[i].age < cache.blocks[oldest].age)
+			oldest = i;
 	}
 	
-	//Emcontrar o bloco mais velho
-	for (unsigned j = i; j < cache_size; j++){
+	 /* All blocks are invalid. */
+	 if (oldest == UINT_MAX)
+		return;
 		
-		//Testar se o block é valido e se é mais velho que o atual
-		if((cache.blocks[j]->flags = VALIDO_LIMPO || cache.blocks[j]->flags = VALIDO_SUJO )
-			&& cache.blocks[j]->age < old){
-				old = cache.blocks[j]->age;
-				old_blocks = j; 
-		}
-	}
-		
-		//Remover da hash
-		/* A função não deveria receber as funções key (retorna chave da hash) e
-		 * cmp (usado para comparar o bloco procurado com os blocos das listas da hash) para serem passadas
-		 * para a função hash_remove ao invés de ter addr no block
-		 */
-		
-		// block_write = hash_remove(&table, cache.blocks[old_blocks], unsigned (*key)(void *), int (*cmp)(void *, void *))
+	/* Remove block from the hash table. */
+	hash_remove(cache.table, cache.blocks[oldest].addr, getkey);
 
+	/* Write block to swap file. */
+	fseek(cache.swp, (cache.blocks[oldest].offset*cache.obj_size), SEEK_SET); 
+	fwrite(cache.blocks[oldest].obj, cache.obj_size, 1, cache.swp);
 		
-		//Gravar no swap
-		
-		// Posicionar o ponteiro do arquivo na posição que o bloco será escrito
-		fseek(cache.swp, (block_write->offset * cache.obj_size), SEEK_SET); 
-		fwrite( block_write, cache.obj_size, 1 , cache.swp);
-		
-		//inserir o bloco da lista de livres
-		list_insert(cache.free, &cache.blocks[old_blocks]);
-	
+	/* Inser block in the list of free blocks. */
+	list_insert(cache.free, &cache.blocks[oldest]);
 }
 
 /**
@@ -152,6 +145,7 @@ void cache_init(FILE *file, size_t obj_size, unsigned cache_size)
 	assert(file != NULL);
 	assert(obj_size > 0);
 	assert(cache_size > 0);
+	assert(cache_size < UINT_MAX);
 	
 	/* Initialize cache. */
 	cache.swp = file;
