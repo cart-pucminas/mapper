8100aad21f96f545d586be18232f31f5c36cfd5f
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-10-27 16:32:23 -0200

    adding greedy to script

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,42 +5,84 @@ NCLUSTERS=4
 MAPPER="bin/mapper --verbose"
 
 #
-# Run kmeans strategy.
+# Runs kmeans strategy.
 #  $1 Number of processes.
 #  $2 Processor topology.
 #  $3 Kernel.
-#  $4 Hierarchical kmeans?
-#  $5 Seed value.
+#  $4 Seed value.
 #
 function run_kmeans
 {	
 	# Build command.
 	topology="--topology $2"
 	infile="--input $INDIR/$3/$1.trace"
-	if [ $4 == "y" ]; then
-		cmd="$MAPPER --seed $5 $topology $infile --hierarchical"
-	else
-		cmd="$MAPPER --seed $5 $topology $infile --nclusters $NCLUSTERS"
-	fi
+	cmd="$MAPPER --seed $4 $topology $infile --kmeans $NCLUSTERS"
 	
 	output=$(($cmd 1> /dev/null) 2>&1)
 	
 	# Print only valid output.
 	if [ $? == "0" ] ; then
-		echo "$4;$3;$1;${output//[[:blank:]]/}"
+		echo "kmeans;$3;$1;${output//[[:blank:]]/}"
 	fi
+}
+
+#
+# Runs hierarchical strategy.
+#  $1 Number of processes.
+#  $2 Processor topology.
+#  $3 Kernel.
+#  $4 Seed value.
+#
+function run_hierarchical
+{	
+	# Build command.
+	topology="--topology $2"
+	infile="--input $INDIR/$3/$1.trace"
+	cmd="$MAPPER --seed $4 $topology $infile --hierarchical"
+	
+	output=$(($cmd 1> /dev/null) 2>&1)
+	
+	# Print only valid output.
+	if [ $? == "0" ] ; then
+		echo "hierarchical;$3;$1;${output//[[:blank:]]/}"
+	fi
+}
+
+#
+# Runs greedy strategy.
+#  $1 Number of processes.
+#  $2 Processor topology.
+#  $3 Kernel.
+#
+function run_greedy
+{	
+	# Build command.
+	topology="--topology $2"
+	infile="--input $INDIR/$3/$1.trace"
+	cmd="$MAPPER $topology $infile --greedy"
+	
+	output=$(($cmd 1> /dev/null) 2>&1)
+	
+	# Print only valid output.
+#	if [ $? == "0" ] ; then
+		echo "greedy;$3;$1;${output//[[:blank:]]/}"
+#	fi
 }
 
 seeds=( 28 1013546 1013546 0 0 )
 kernels=( CG EP FT IS MG )
 
 for i in {0..4}; do
-	run_kmeans 32    4x8 ${kernels[$i]} n ${seeds[$i]}
-	run_kmeans 32    4x8 ${kernels[$i]} y ${seeds[$i]}
-	run_kmeans 64    8x8 ${kernels[$i]} n ${seeds[$i]}
-	run_kmeans 64    8x8 ${kernels[$i]} y ${seeds[$i]}
-	run_kmeans 128  8x16 ${kernels[$i]} n ${seeds[$i]}
-	run_kmeans 128  8x16 ${kernels[$i]} y ${seeds[$i]}
-	run_kmeans 256 16x16 ${kernels[$i]} n ${seeds[$i]}
-	run_kmeans 256 16x16 ${kernels[$i]} y ${seeds[$i]}
+	run_kmeans        32   4x8 ${kernels[$i]} ${seeds[$i]}
+	run_hierarchical  32   4x8 ${kernels[$i]} ${seeds[$i]}
+	run_greedy        32   4x8 ${kernels[$i]}
+	run_kmeans        64   8x8 ${kernels[$i]} ${seeds[$i]}
+	run_hierarchical  64   8x8 ${kernels[$i]} ${seeds[$i]}
+	run_greedy        64   8x8 ${kernels[$i]}
+	run_kmeans       128  8x16 ${kernels[$i]} ${seeds[$i]}
+	run_hierarchical 128  8x16 ${kernels[$i]} ${seeds[$i]}
+	run_greedy       128  8x16 ${kernels[$i]}
+	run_kmeans       256 16x16 ${kernels[$i]} ${seeds[$i]}
+	run_hierarchical 256 16x16 ${kernels[$i]} ${seeds[$i]}
+	run_greedy       256 16x16 ${kernels[$i]}
 done
