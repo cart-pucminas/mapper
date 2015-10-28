780b49b4a44928ae0e11d5674628e170058af724
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2015-10-27 23:25:53 -0200

    updating examples

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 INDIR=examples
+OUTDIR=output
 NCLUSTERS=4
 MAPPER="bin/mapper --verbose"
 
@@ -12,18 +13,22 @@ MAPPER="bin/mapper --verbose"
 #  $4 Seed value.
 #
 function run_kmeans
-{	
+{
+	cut -d" " -f2- $INDIR/$3/$1.trace > input
+	
 	# Build command.
 	topology="--topology $2"
-	infile="--input $INDIR/$3/$1.trace"
+	infile="--input input"
 	cmd="$MAPPER --seed $4 $topology $infile --kmeans $NCLUSTERS"
 	
-	output=$(($cmd 1> /dev/null) 2>&1)
+	output=$(($cmd 1> $OUTDIR/kmeans-$1-$3.map) 2>&1)
 	
 	# Print only valid output.
 	if [ $? == "0" ] ; then
 		echo "kmeans;$3;$1;${output//[[:blank:]]/}"
 	fi
+	
+	rm -f input
 }
 
 #
@@ -34,18 +39,22 @@ function run_kmeans
 #  $4 Seed value.
 #
 function run_hierarchical
-{	
+{
+	cut -d" " -f2- $INDIR/$3/$1.trace > input
+	
 	# Build command.
 	topology="--topology $2"
-	infile="--input $INDIR/$3/$1.trace"
+	infile="--input input"
 	cmd="$MAPPER --seed $4 $topology $infile --hierarchical"
 	
-	output=$(($cmd 1> /dev/null) 2>&1)
+	output=$(($cmd 1> $OUTDIR/kmeans-$1-$3.map) 2>&1)
 	
 	# Print only valid output.
 	if [ $? == "0" ] ; then
 		echo "hierarchical;$3;$1;${output//[[:blank:]]/}"
 	fi
+	
+	rm -f input
 }
 
 #
@@ -55,22 +64,29 @@ function run_hierarchical
 #  $3 Kernel.
 #
 function run_greedy
-{	
+{
+	cut -d" " -f2- $INDIR/$3/$1.trace > input
+	
 	# Build command.
 	topology="--topology $2"
-	infile="--input $INDIR/$3/$1.trace"
+	infile="--input input"
 	cmd="$MAPPER $topology $infile --greedy"
 	
-	output=$(($cmd 1> /dev/null) 2>&1)
+	output=$(($cmd 1> $OUTDIR/kmeans-$1-$3.map) 2>&1)
 	
 	# Print only valid output.
 	if [ $? == "0" ] ; then
 		echo "greedy;$3;$1;${output//[[:blank:]]/}"
 	fi
+	
+	rm -f input
 }
 
 seeds=( 28 1013546 1013546 0 0 )
 kernels=( CG EP FT IS MG )
+
+mkdir -p $OUTDIR
+rm -rf $OUTDIR/*
 
 for i in {0..4}; do
 	run_kmeans        32   4x8 ${kernels[$i]} ${seeds[$i]}
